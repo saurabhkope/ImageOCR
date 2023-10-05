c175d21ca3964f9e33540cbe4753642487feccb5
saurabhkope <122842208+saurabhkope@users.noreply.github.com>
2023-10-05 21:38:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 async function performOCR() {
     const imageInput = document.getElementById('imageInput');
     const resultElement = document.getElementById('result');
-
+    resultElement.textContent = 'Performing OCR... Please wait.';
     if (imageInput.files.length === 0) {
         resultElement.textContent = 'Please select an image.';
         return;
     }
-
     const imageFile = imageInput.files[0];
     const { data: { text } } = await Tesseract.recognize(imageFile);
     resultElement.textContent = 'Extracted Text: ' + text;
